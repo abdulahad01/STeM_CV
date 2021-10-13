6eb2a0407e4999d656ed0a3cbfdab79a87408533
abdulahad01 <abdudeturk@gmail.com>
2021-10-13 21:59:32 +0530

    Corrected the check condition and added debugging statements

--- master.py
+++ master.py
@@ -12,15 +12,16 @@ numb =1
 
 print('Enter 1 for ON or 0 for OFF')
 
-while numb ==1:
+   while numb ==1:
 
     isMaskSet = main()
 
     ledstate = isMaskSet
+    print(ledstate)
     
-    if ledstate == '1':
+    if ledstate == 1:
         bus.write_byte(addr, 0x1)
-    elif ledstate == '0':
+    elif ledstate == 0:
         bus.write_byte(addr, 0x0)
     else:
         numb = 0
